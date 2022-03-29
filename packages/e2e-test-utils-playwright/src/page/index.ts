4374aefc75a4590ed2b88c5684f178f5bb6db0d1
juhi saxena <juhi.saxena@rtcamp.com>
2022-03-29 14:34:55 +0530

    Migrate buttons test to Playwright

--- packages/e2e-test-utils-playwright/src/page/index.ts
+++ packages/e2e-test-utils-playwright/src/page/index.ts
@@ -9,6 +9,13 @@ import type { Browser, Page, BrowserContext } from '@playwright/test';
 import { getPageError } from './get-page-error';
 import { isCurrentURL } from './is-current-url';
 import { visitAdminPage } from './visit-admin-page';
+import { pressKeyWithModifier } from './press-key-with-modifier';
+import { insertBlock } from './insert-block';
+import { canvas } from './canvas';
+import { wpDataSelect } from './wp-data-select';
+import { createNewPost } from './create-new-post';
+import { clickBlockAppender } from './click-block-appender';
+import { getEditedPostContent } from './get-edited-post-content';
 
 class PageUtils {
 	browser: Browser;
@@ -24,6 +31,13 @@ class PageUtils {
 	getPageError = getPageError;
 	isCurrentURL = isCurrentURL;
 	visitAdminPage = visitAdminPage;
+	pressKeyWithModifier = pressKeyWithModifier;
+	insertBlock = insertBlock;
+	canvas = canvas;
+	wpDataSelect = wpDataSelect;
+	createNewPost = createNewPost;
+	clickBlockAppender = clickBlockAppender;
+	getEditedPostContent = getEditedPostContent;
 }
 
 export { PageUtils };
